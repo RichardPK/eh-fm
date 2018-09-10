f9e27371115f8b61bc17eda48dea9e2dc6353ab6
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-09-10 15:47:41 +0100

    Redesigned state & asynchronous API requests, now working way better & props being passed down correctly

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import ScheduleContainer from './ScheduleContainer'
+import _ from 'lodash';
 
 class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
       showSchedule: [],
-      currentDate: null
+      currentDate: null,
+      selectedDay: null,
+      displayedDays: []
     }
     this.apiCall = this.apiCall.bind(this);
     this.fetchDate = this.fetchDate.bind(this);
+    this.populateSchedule = this.populateSchedule.bind(this);
+    this.convertShowScheduleToArray = this.convertShowScheduleToArray.bind(this);
+    this.deleteDaysInPast = this.deleteDaysInPast.bind(this);
   }
 
   componentDidMount(){
     this.apiCall();
-    this.fetchDate();
+
   }
 
   apiCall(){
     fetch('https://ehfm.airtime.pro/api/week-info')
     .then(response => response.json())
-    .then(data => this.setState({ showSchedule: data }))
-    .then(this.apiDataLoaded());
+    .then(data => this.setState({ showSchedule: data }, function(){
+      this.populateSchedule()
+    }))
+    .then(this.fetchDate())
+    // .then(this.populateSchedule())
+    .then(this.apiDataLoaded())
   }
 
   fetchDate(){
@@ -29,14 +39,12 @@ class Main extends Component {
     let dd = today.getDate();
     let mm = today.getMonth()+1; //January is 0!
     let yyyy = today.getFullYear();
-
     if(dd<10) {
       dd = '0'+dd
     }
     if(mm<10) {
       mm = '0'+mm
     }
-
     today = yyyy + '-' + mm + '-' + dd;
     this.setState({currentDate: today})
   }
@@ -45,13 +53,55 @@ class Main extends Component {
     console.log("loaded");
   }
 
+  populateSchedule(){
+    let showArray = this.convertShowScheduleToArray();
+    let nextSevenDaysSchedule = this.deleteDaysInPast(showArray);
+    this.setState({selectedDay: nextSevenDaysSchedule[0]})
+
+    const allShowDays = nextSevenDaysSchedule.map((day, index) => {
+      return <th key={index}>{day[0]}</th>
+    })
+    this.setState({displayedDays: allShowDays});
+  }
+
+  convertShowScheduleToArray(){
+    let showSchedule = this.state.showSchedule;
+    let showScheduleArray = [];
+
+    Object.keys(showSchedule).forEach(function(key){
+      showScheduleArray.push(key, showSchedule[key]);
+      // console.log(key, showSchedule[key]);
+    })
+    let newArray = _.chunk(showScheduleArray, 2)
+    let arrayLength = newArray.length;
+    let positionToRemove = arrayLength - 1;
+
+    newArray.splice(positionToRemove, 1)
+    return newArray;
+  }
+
+  deleteDaysInPast(scheduleData){
+    let currentDate = this.state.currentDate;
+
+    for (let day of scheduleData){
+      if (day[1].length !== 0){
+
+        if(day[1][0].start_timestamp.includes(currentDate)){
+          let currentDayInScheduleIndex = scheduleData.indexOf(day);
+          let finalDayInScheduleToDisplay = currentDayInScheduleIndex + 7;
+          let nextSevenDaysSchedule = scheduleData.splice(currentDayInScheduleIndex, finalDayInScheduleToDisplay);
+          return nextSevenDaysSchedule;
+        }
+      }
+    }
+  }
+
   render(){
     return(
       <React.Fragment>
-        <p>Main</p>
         <ScheduleContainer
-          showSchedule={this.state.showSchedule}
-          currentDate={this.state.currentDate}
+          daysToDisplay={this.state.displayedDays}
+          selectedDay={this.state.selectedDay}
         />
       </React.Fragment>
     )
--- src/containers/ScheduleContainer.js
+++ src/containers/ScheduleContainer.js
@@ -4,59 +4,6 @@ import _ from 'lodash';
 class ScheduleContainer extends Component {
   constructor(props){
     super(props);
-    this.populateSchedule = this.populateSchedule.bind(this);
-    this.convertShowScheduleToArray = this.convertShowScheduleToArray.bind(this);
-    this.deleteDaysInPast = this.deleteDaysInPast.bind(this);
-  }
-
-  populateSchedule(){
-
-    if(this.props.showSchedule.length !== 0){
-      let showArray = this.convertShowScheduleToArray();
-      // console.log(showArray);
-      let nextSevenDaysSchedule = this.deleteDaysInPast(showArray);
-      const allShowDays = nextSevenDaysSchedule.map((show, index) => {
-        return <th key={index}>{show[0]}</th>
-      })
-      return allShowDays;
-    } else {
-      return <p>Loading</p>
-    }
-  }
-
-  convertShowScheduleToArray(){
-    let showSchedule = this.props.showSchedule;
-    let showScheduleArray = [];
-
-    Object.keys(showSchedule).forEach(function(key){
-      showScheduleArray.push(key, showSchedule[key]);
-      // console.log(key, showSchedule[key]);
-    })
-    let newArray = _.chunk(showScheduleArray, 2)
-    // console.log(showScheduleArray);
-    // console.log(newArray);
-
-    let arrayLength = newArray.length;
-    let positionToRemove = arrayLength - 1;
-
-    newArray.splice(positionToRemove, 1)
-    return newArray;
-  }
-
-  deleteDaysInPast(scheduleData){
-    let currentDate = this.props.currentDate;
-
-    for (let day of scheduleData){
-      if (day[1].length !== 0){
-
-        if(day[1][0].start_timestamp.includes(currentDate)){
-          let currentDayInScheduleIndex = scheduleData.indexOf(day);
-          let finalDayInScheduleToDisplay = currentDayInScheduleIndex + 7;
-          let nextSevenDaysSchedule = scheduleData.splice(currentDayInScheduleIndex, finalDayInScheduleToDisplay);
-          return nextSevenDaysSchedule;
-        }
-      }
-    }
   }
 
   render(){
@@ -65,7 +12,7 @@ class ScheduleContainer extends Component {
         <table>
           <tbody>
             <tr>
-              {this.populateSchedule()}
+              {this.props.daysToDisplay}
             </tr>
           </tbody>
         </table>
