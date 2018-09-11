e4e4c088f3a63412e604993472ea33db2068e068
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-09-11 16:55:21 +0100

    Audio ref setup for full audio player manipulation

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Player.css';
 
-const Player = (props) => {
+class Player extends Component {
+  constructor(props){
+    super(props)
+    this.audioPlayer = React.createRef();
+  }
+
+  componentDidMount(){
+    console.log(this.audioPlayer);
+  }
+
+  render(){
+
+    return(
+      <React.Fragment>
+        <audio ref={this.audioPlayer} id='audioPlayer' controls name="media">
+          <source src="http://ehfm.out.airtime.pro:8000/ehfm_a" type="audio/mpeg"/>
+        </audio>
+      </React.Fragment>
+    )
 
 
-  return(
-    <React.Fragment>
-      <audio controls name="media">
-        <source src="http://ehfm.out.airtime.pro:8000/ehfm_a" type="audio/mpeg"/>
-      </audio>
-    </React.Fragment>
-  )
+  }
 
 }
 
