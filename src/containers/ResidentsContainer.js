572c546e04b4e3cd6955a39275a1055846344dc4
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-10-01 15:19:01 +0100

    Prismic API call working

--- src/containers/ResidentsContainer.js
+++ src/containers/ResidentsContainer.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import Prismic from 'prismic-javascript';
 import './ResidentsContainer.css'
 
 class ResidentsContainer extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      prismicDoc: null
+    }
+  }
+
+  componentDidMount(){
+    console.log("Resident component mounted");
+    const apiEndpoint = "https://testreporpk.cdn.prismic.io/api/v2";
+
+    Prismic.api(apiEndpoint)
+    .then(api => {
+      api.query(Prismic.Predicates.at('document.type', 'show')).then(response => {
+        if (response) {
+          this.setState({ prismicDoc: response.results });
+        }
+      });
+    });
   }
 
   render(){
     return(
       <div className="residents-container">
-      <p>Hello</p>
+        <p>Hello</p>
       </div>
     )
   }
