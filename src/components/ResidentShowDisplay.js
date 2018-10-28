3ccd9112df8281d0c02101467b6a186382eb53f5
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-10-28 16:19:29 +0000

    Full screen images for resident pages

--- src/components/ResidentShowDisplay.js
+++ src/components/ResidentShowDisplay.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./ResidentShowDisplay.scss";
 
-const ResidentShowDisplay = (props) => {
+const ResidentShowDisplay = props => {
+  return (
+    <React.Fragment>
+      <div className="resident-show-bg-img">
+        <img src={props.showImage} alt={props.showTitle} />
+      </div>
 
-  return(
-    <div className="resident-show-display-container">
-      <h1>{props.showTitle}</h1>
-      <p>{props.showDescription}</p>
-    </div>
-  )
+      <div className="resident-show-display-container">
+        <h1>{props.showTitle}</h1>
+        <p>{props.showDescription}</p>
+      </div>
 
-}
+    </React.Fragment>
+  );
+};
 
 export default ResidentShowDisplay;
