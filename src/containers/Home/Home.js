61d1408fa6472c0b6ccd6f5c65b8a622a4126e4d
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2019-08-21 19:58:30 +0100

    Added helmet to Home container

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import CurrentShow from '../../components/CurrentShow/CurrentShow';
-import Schedule from '../../components/Schedule/Schedule';
+import React from "react";
+import CurrentShow from "../../components/CurrentShow/CurrentShow";
+import Schedule from "../../components/Schedule/Schedule";
+import { Helmet } from "react-helmet";
 
 const HomeContainer = (props) => {
-
-  return(
+  return (
     <React.Fragment>
+      <Helmet>
+        <title>EH-FM</title>
+        <meta property="og:title" content="EH-FM" />
+        <meta name="description" content="Independent community radio for Edinburgh" />
+        <meta property="og:description" content="Independent community radio for Edinburgh" />
+        <meta property="og:url" content="http://www.ehfm.live" />
+        <meta name="twitter:image" content="https://www.ehfm.live/placeholder-showimg.jpg" />
+        <meta name="twitter:image" content="https://www.ehfm.live/placeholder-showimg.jpg" />
+      </Helmet>
 
-      <div className="body-container" 
-      style={{ marginBottom: (props.mixCloudWidget ? '123px' : null) }}>
-
+      <div className="body-container" style={{ marginBottom: props.mixCloudWidget ? "123px" : null }}>
         <CurrentShow
           currentShow={props.currentShow}
           playing={props.playing}
-          handlePlayPauseClicked = {props.handlePlayPauseClicked}
+          handlePlayPauseClicked={props.handlePlayPauseClicked}
         />
 
-        <Schedule
-          daysToDisplay={props.daysToDisplay}
-          selectedDay={props.selectedDay}
-        />
-
+        <Schedule daysToDisplay={props.daysToDisplay} selectedDay={props.selectedDay} />
       </div>
-
     </React.Fragment>
-  )
-
-}
+  );
+};
 
 export default HomeContainer;
