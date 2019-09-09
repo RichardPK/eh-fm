dd43e98d7fd55051cc993c624ad28bf02be805a4
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2019-09-09 22:13:14 +0100

    Updated image routes to public folder

--- src/components/NavBar/Logo/Logo.js
+++ src/components/NavBar/Logo/Logo.js
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import './Logo.scss'
+import "./Logo.scss";
+import Logo from "../../../assets/images/EHFM_Arched-Logo_Teal_1080.png";
 
 const LogoHead = (props) => {
-
-  return(
-
+  return (
     <React.Fragment>
       <div className="logo-head">
         <Link to="/">
-        <img className="arched-logo" src="https://www.ehfm.live//EHFM_Arched-Logo_Teal_1080.png" alt="ehfm arched logo"/>
-      </Link>
-    </div>
-  </React.Fragment>
-
-)
-}
+          <img className="arched-logo" src={Logo} alt="ehfm arched logo" />
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default LogoHead;
