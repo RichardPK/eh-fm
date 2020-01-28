f3703da03fe1e4644693c7db91884ed5c4bfa11e
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-01-28 21:44:56 +0000

    Migrated Schedule inner to new

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import "./Schedule.scss";
-import styled from "styled-components";
-import Devices from "../../consts/Devices";
-import { Heading4, Heading3, Body } from "../text-elements/index";
-import Colors from "../../consts/Colors";
+import React, { Component } from 'react';
+import './Schedule.scss';
+import styled from 'styled-components';
+import Devices from '../../consts/Devices';
+import { Heading4, Heading3, Body } from '../text-elements/index';
+import Colors from '../../consts/Colors';
 
 class Schedule extends Component {
   constructor(props) {
@@ -32,7 +32,7 @@ class Schedule extends Component {
   showNameParser(show) {
     let currentShowName = show.name;
     let parsedForInvertedCommas = currentShowName.replace(/&#039;/g, "'");
-    let parsedForAmpersands = parsedForInvertedCommas.replace(/&amp;/g, "&");
+    let parsedForAmpersands = parsedForInvertedCommas.replace(/&amp;/g, '&');
     return parsedForAmpersands;
   }
 
@@ -40,12 +40,12 @@ class Schedule extends Component {
     let startTime = show.start_timestamp;
     let endTime = show.end_timestamp;
     let parsedStart = startTime
-      .split(" ")
+      .split(' ')
       .splice(1)
       .join()
       .slice(0, -3);
     let parsedEnd = endTime
-      .split(" ")
+      .split(' ')
       .splice(1)
       .join()
       .slice(0, -3);
@@ -58,20 +58,16 @@ class Schedule extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <Wrapper>
-          <Heading4Component>Schedule</Heading4Component>
-          <div className="schedule-container">
-            <div className="schedule-subcontainer">
-              <div className="days-header">{this.props.daysToDisplay}</div>
-              <div className="schedule-divider"></div>
-              <table className="show-schedule">
-                <tbody>{this.renderSelectedDay()}</tbody>
-              </table>
-            </div>
-          </div>
-        </Wrapper>
-      </React.Fragment>
+      <Wrapper>
+        <Heading4Component>Schedule</Heading4Component>
+        <Inner>
+          <div className="days-header">{this.props.daysToDisplay}</div>
+          <div className="schedule-divider"></div>
+          <table className="show-schedule">
+            <tbody>{this.renderSelectedDay()}</tbody>
+          </table>
+        </Inner>
+      </Wrapper>
     );
   }
 }
@@ -82,7 +78,6 @@ const Wrapper = styled.div`
   width: 85vw;
   margin: 0px 10px 0px 10px;
   overflow: hidden;
-  box-shadow: 0px 1px 10px 0.1px rgba(0, 0, 0, 0.05);
   border-radius: 5px;
 
   @media ${Devices.tablet} {
@@ -95,4 +90,17 @@ const Heading4Component = styled(Heading4)`
   color: ${Colors.notQuiteBlack};
   margin: 10px 20px 10px 10px;
 `;
+
+const Inner = styled.div`
+  background-color: white;
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  /* width: 41vw; */
+  margin: 0px 20px 0px 10px;
+  overflow: hidden;
+  box-shadow: 0px 1px 10px 0.1px rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+`;
+
 export default Schedule;
