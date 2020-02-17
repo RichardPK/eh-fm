13e25bbacf4b1d4dccc11c8910396e80d03e5020
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-02-17 21:56:26 +0000

    Playcolours changing based on play state

--- src/components/Player/now-playing/NowPlaying.js
+++ src/components/Player/now-playing/NowPlaying.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import Playbutton from '../Playbutton/Playbutton';
+import Devices from '../../../consts/Devices';
+import Colors from '../../../consts/Colors';
 
 const NowPlaying = ({ handlePlayPauseClicked, playing, currentShow }) => {
   const renderPlayingContainer = () => {
@@ -24,15 +26,67 @@ const NowPlaying = ({ handlePlayPauseClicked, playing, currentShow }) => {
   };
 
   return (
-    <div className={renderPlayingContainer()} onClick={handlePlayPauseClicked}>
+    <Wrapper playing={playing} onClick={handlePlayPauseClicked}>
       <div className="play-button-container">
         <Playbutton playingTrueFalse={playing} playClicked={handlePlayPauseClicked} />
       </div>
       <p className="current-show">{returnShowData()}</p>
-    </div>
+    </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px 8px 8px 8px;
+  cursor: pointer;
+  background-color: ${(props) => (props.playing ? Colors.playerWhite : Colors.ehfmPrimary)};
+
+  p {
+    color: ${(props) => (props.playing ? Colors.altBlue : Colors.playerWhite)};
+  }
+
+  .play-button {
+    border-color: transparent transparent transparent
+      ${(props) => (props.playing ? Colors.altBlue : Colors.playerWhite)};
+  }
+
+  .pause-button {
+    border-color: ${Colors.altBlue};
+  }
+
+  @media ${Devices.tablet} {
+    &:hover {
+      background-color: ${Colors.playerWhite};
+      cursor: pointer;
+
+      p {
+        color: ${Colors.altBlue};
+      }
+
+      .play-button {
+        border-color: transparent transparent transparent ${Colors.altBlue};
+      }
+
+      .pause-button {
+        border-color: ${Colors.altBlue};
+      }
+    }
+  }
+`;
+
+// background-color: $playerwhite;
+
+// // p {
+// //   color: $altblue;
+// // }
+
+// // .play-button {
+// //   border-color: transparent transparent transparent $altblue;
+// // }
+
+// // .pause-button {
+// //   border-color: $altblue;
+// // }
 
 export default NowPlaying;
