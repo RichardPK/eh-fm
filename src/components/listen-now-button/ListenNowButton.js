290c68259eeb041b828842b67692713bc4a66d5b
RichardPK <r.phillips.kerr@gmail.com>
2020-02-23 12:24:31 +0000

    ListenNow button styled and current show scss deleted

--- src/components/listen-now-button/ListenNowButton.js
+++ src/components/listen-now-button/ListenNowButton.js
@@ -1,22 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 import PlayPauseButton from "../player/play-pause-button/PlayPauseButton";
+import Colors from "../../consts/Colors";
+import Devices from "../../consts/Devices";
 
 const ListenNowButton = ({ playing, playClicked }) => {
-  const renderPlayingContainer = () => {
-    if (playing === true) {
-      return "currentshow-playbutton-container-white";
-    } else {
-      return "currentshow-playbutton-container";
-    }
-  };
-
   return (
-    <div className={renderPlayingContainer()} onClick={playClicked}>
+    <Wrapper
+      className={"currentshow-playbutton-container"}
+      onClick={playClicked}
+    >
       <p>Listen now</p>
       <PlayPauseButton playingTrueFalse={playing} playClicked={playClicked} />
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  cursor: pointer;
+  padding: 4px;
+  padding: 10px;
+  background-color: ${Colors.altBlue};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 140px;
+  letter-spacing: 1px;
+
+  p {
+    margin: 0px;
+  }
+
+  @media ${Devices.tablet} {
+    &:hover {
+      background-color: ${Colors.playerWhite};
+      color: ${Colors.altBlue};
+
+      .play-button {
+        border-color: transparent transparent transparent ${Colors.altBlue};
+      }
+
+      .pause-button {
+        border-color: ${Colors.altBlue};
+      }
+    }
+  }
+  @media ${Devices.mobileS} {
+    padding: 5px;
+  }
+
+  @media ${Devices.mobileL} {
+    font-size: 14px;
+    padding: 10px;
+  }
+`;
 
 export default ListenNowButton;
