e10a72bca5d7713128b67bd55628b0f7c61bf351
RichardPK <r.phillips.kerr@gmail.com>
2020-02-23 14:23:50 +0000

    Resident list refactor complete

--- src/components/resident-list-item/ResidentListItem.js
+++ src/components/resident-list-item/ResidentListItem.js
@@ -1,25 +1,77 @@
 import React from "react";
-import "./ResidentListItem.scss";
-// import ResidentShowContainer from '../containers/ResidentShowContainer'
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+import Colors from "../../consts/Colors";
+import Devices from "../../consts/Devices";
+import { Cta } from "../text-elements/index";
 
 const ResidentListItem = props => {
-  const renderShowPageUrl = function() {
-    return "/residents/" + props.showId;
-  };
-
   return (
     <React.Fragment>
-      <Link to={renderShowPageUrl()}>
-        <div className="resident-list-item">
-          <div className="resident-list-item-image">
-            <img src={props.thumbnailImage} alt="show"></img>
-          </div>
-          <h4 className="resident-list-item-title">{props.showTitle}</h4>
-        </div>
+      <Link to={`/residents/${props.showId}`}>
+        <Wrapper>
+          <ImageWrapper>
+            <ShowImage src={props.thumbnailImage} alt="show presenters" />
+          </ImageWrapper>
+          <ShowTitle>{props.showTitle}</ShowTitle>
+        </Wrapper>
       </Link>
     </React.Fragment>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 265px;
+  padding: 5px 5px 4px 5px;
+  margin: 15px 5px;
+  background: white;
+  border-bottom: 2px solid ${Colors.notQuiteBlack};
+  height: 190px;
+  width: 70vw;
+
+  @media ${Devices.mobileL} {
+    height: 140px;
+    width: 30vw;
+    margin: 15px 10px 15px 15px;
+  }
+
+  @media ${Devices.tablet} {
+    height: 190px;
+    width: 20vw;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+  overflow: hidden;
+
+  @media ${Devices.tablet} {
+    &:hover {
+      img {
+        opacity: 0.8;
+      }
+    }
+  }
+`;
+
+const ShowImage = styled.img`
+  width: auto;
+  height: 265px;
+  margin: 0px 0px 0px 0px;
+  opacity: 1;
+  transition: opacity, 0.2s ease-out;
+`;
+
+const ShowTitle = styled(Cta)`
+  padding-top: 15px;
+  color: ${Colors.notQuiteBlack};
+  font-weight: normal;
+  letter-spacing: 1px;
+`;
 
 export default ResidentListItem;
