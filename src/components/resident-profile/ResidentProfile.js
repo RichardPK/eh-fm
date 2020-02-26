0f6cf6d61e9f259ed6722e4f76c6ed1d178438d0
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-02-26 21:44:03 +0000

    rm residents scss

--- src/components/resident-profile/ResidentProfile.js
+++ src/components/resident-profile/ResidentProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import './ResidentProfile.scss';
+
 import styled from 'styled-components';
 import renderHTML from 'react-render-html';
 import {
