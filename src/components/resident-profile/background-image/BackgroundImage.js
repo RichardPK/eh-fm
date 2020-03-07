f3e878e85dd959d3991adac932225dd7eab94c35
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-03-07 10:46:59 +0000

    Moved background image into its own comp

---
+++ src/components/resident-profile/background-image/BackgroundImage.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
+import Devices from '../../../consts/Devices';
+
+const BackgroundImage = ({ mixCloudWidget, showImage }) => {
+  return <Wrapper mixCloudWidget={mixCloudWidget} showImage={showImage} />;
+};
+
+const Wrapper = styled.div`
+  background-position: center center !important;
+  background-size: cover !important;
+  width: 100vw;
+  height: calc(100vh - 100px);
+  display: flex;
+  overflow: hidden;
+  position: absolute;
+  z-index: -100;
+  top: 100px;
+  left: 0;
+  margin-bottom: ${(props) => (props.mixCloudWidget ? '123px' : 'auto')};
+  background-image: url(${(props) => props.showImage});
+
+  img {
+    max-width: 100%;
+    margin: auto;
+  }
+
+  @media ${Devices.mobile} {
+  }
+`;
+
+export default BackgroundImage;
