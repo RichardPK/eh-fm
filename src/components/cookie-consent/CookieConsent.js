79b6d88e9b4c1316cc5871ccf489b33c4003ba6b
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-03-29 18:06:49 +0100

    Stuff

--- src/components/cookie-consent/CookieConsent.js
+++ src/components/cookie-consent/CookieConsent.js
@@ -16,16 +16,25 @@ const CookieConsent = ({}) => {
 
   return (
     <Wrapper>
-      <CookiesText>
-        🍪 This website uses cookies to help us make the eh-fm better. You can read more about how,{' '}
-        {<a href="https://www.iubenda.com/">here</a>}.
-      </CookiesText>
-      {/* <CookiesButton positive onClick={() => handleCookieConsentClick('positive')}>
+      <Inner>
+        <CookiesText>
+          🍪 This website uses cookies to help us make eh-fm better. You can read more about how,{' '}
+          {<a href="https://www.iubenda.com/">here</a>}.
+        </CookiesText>
+        {/* <CookiesButton positive onClick={() => handleCookieConsentClick('positive')}>
         I'm OK with that
       </CookiesButton>
       <CookiesButton negative onClick={() => handleCookieConsentClick('negative')}>
         Not OK
       </CookiesButton> */}
+        <CloseButton
+          onClick={() => {
+            console.log('Cross clicked');
+          }}
+        >
+          <span>x</span>
+        </CloseButton>
+      </Inner>
     </Wrapper>
   );
 };
@@ -34,13 +43,16 @@ const Wrapper = styled.div`
   position: fixed;
   top: 154px;
   z-index: 2;
+  width: 100vw;
 
   @media ${Devices.tablet} {
     top: 126px;
   }
+`;
 
-  width: 100%;
+const Inner = styled.div`
   display: flex;
+  justify-content: space-between;
   background-color: ${Colors.playerWhiteCustom(0.95)};
   padding: 1rem 30px;
 `;
@@ -51,6 +63,23 @@ const CookiesText = styled(Body)`
     color: ${Colors.ehfmPrimary};
     text-decoration: underline;
   }
+`;
+
+const CloseButton = styled.div`
+  text-align: center;
+  align-self: flex-end;
+  font-size: 14px;
+  padding: 1px 6px 4px 7px;
+  color: ${Colors.ehfmPrimary};
+  cursor: pointer;
+/* 
+  @media ${Devices.tablet} {
+    &:hover {
+      background-color: ${Colors.softWhite};
+      color: ${Colors.altBlue60Transparent};
+      transition: background-color 0.2s, color 0.2s, ease-out;
+    }
+  } */
 `;
 
 export default CookieConsent;
