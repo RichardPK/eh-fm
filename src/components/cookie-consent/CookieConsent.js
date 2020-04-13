e46b89e2f98d5999e05a0a297fd8cfbd8ceb0084
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-04-13 12:12:19 +0100

    Styled for mobile

--- src/components/cookie-consent/CookieConsent.js
+++ src/components/cookie-consent/CookieConsent.js
@@ -77,10 +77,14 @@ const Inner = styled.div`
 const WhiteBox = styled.div`
   position: relative;
   display: flex;
-  margin: 0px 40px;
+  margin: 0;
   border-top-left-radius: 4px;
   border-top-right-radius: 4px;
   background-color: ${Colors.playerWhiteCustom(0.95)};
+
+  @media ${Devices.tablet} {
+    margin: 0 40px;
+  }
 `;
 
 const CookiesText = styled(Body)`
@@ -95,21 +99,18 @@ const CookiesText = styled(Body)`
 const CloseButton = styled.div`
   position: absolute;
   top: 1rem;
-  right: 0;
+  right: 10px;
   text-align: center;
   align-self: flex-end;
   font-size: 14px;
   padding: 1px 6px 4px 7px;
   color: ${Colors.ehfmPrimary};
   cursor: pointer;
-/* 
+
   @media ${Devices.tablet} {
-    &:hover {
-      background-color: ${Colors.softWhite};
-      color: ${Colors.altBlue60Transparent};
-      transition: background-color 0.2s, color 0.2s, ease-out;
-    }
-  } */
+    top: 1rem;
+    right: 0;
+  }
 `;
 
 export default CookieConsent;
