6851a58a8e5b53216ca900dbc7683926bf9c00de
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-05-11 21:48:46 +0100

    Added chatango

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MixcloudWidget from './mixcloud-widget/MixcloudWidget';
 import CookieConsent from '../../components/cookie-consent/CookieConsent';
+import Chatango from '../../components/chatango/Chatango';
 
 const FooterContainer = (props) => {
   return (
     <React.Fragment>
       <CookieConsent />
       <MixcloudWidget />
+      <Chatango />
     </React.Fragment>
   );
 };
