2cc49945971b0302e9ee793c1bc3fd97e39ddf2c
RichardPK <r.phillips.kerr@gmail.com>
2020-06-12 13:55:15 +0100

    Reinstate metadata addition

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -9,6 +9,7 @@ import Devices from "../../consts/Devices";
 import Sizes from "../../consts/Sizes";
 import PlaceholderShowImg from "../../assets/images/placeholder-showimg.jpg";
 import Carousel from "../../components/carousel/Carousel";
+import MetaData from "../../components/metadata/MetaData";
 
 const HomeContainer = (props) => {
   const apiEndpoint = "https://ehfm.cdn.prismic.io/api/v2";
@@ -66,36 +67,14 @@ const HomeContainer = (props) => {
 
   return (
     <React.Fragment>
-      <Helmet>
-        <title>EH-FM</title>
-        <meta name="fragment" content="!" />
-        <meta property="og:title" data-react-helmet="true" content="EH-FM" />
-        <meta
-          name="description"
-          data-react-helmet="true"
-          content="EH-FM is an Edinburgh-based online radio station, providing a platform for the capital's local artists and broadcasting 24 hours a day."
-        />
-        <meta
-          property="og:description"
-          data-react-helmet="true"
-          content="EH-FM is an Edinburgh-based online radio station, providing a platform for the capital's local artists and broadcasting 24 hours a day."
-        />
-        <meta
-          property="og:url"
-          data-react-helmet="true"
-          content="https://www.ehfm.live"
-        />
-        <meta
-          property="og:image"
-          data-react-helmet="true"
-          content={PlaceholderShowImg}
-        />
-        <meta
-          name="twitter:image"
-          data-react-helmet="true"
-          content={PlaceholderShowImg}
-        />
-      </Helmet>
+      <MetaData
+        title={"EH-FM | Edinburgh Community Radio broadcast"}
+        url={"https://www.ehfm.live"}
+        image={PlaceholderShowImg}
+        description={
+          "EH-FM is an Edinburgh-based online radio station, providing a platform for the capital's local artists and broadcasting 24 hours a day."
+        }
+      />
 
       <Wrapper
         mixCloudWidget={props.mixCloudWidget}
