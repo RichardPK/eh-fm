d158416215ff44afb0e2be3d1fb490d66ee2a28d
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-08-08 11:58:05 +0100

    Rough grid implementation for past shows

--- src/components/resident-profile/past-show-card/PastShowCard.js
+++ src/components/resident-profile/past-show-card/PastShowCard.js
@@ -1,32 +1,25 @@
-import React from "react";
-import styled from "styled-components/macro";
-import Colors from "../../../consts/Colors";
-import Devices from "../../../consts/Devices";
-import { Body, Heading4 } from "../../text-elements/index";
-import ShowTag from "../show-tag/ShowTag";
+import React from 'react';
+import styled from 'styled-components/macro';
+import Colors from '../../../consts/Colors';
+import Devices from '../../../consts/Devices';
+import { Body, Heading4 } from '../../text-elements/index';
+import ShowTag from '../show-tag/ShowTag';
 
 const PastShowCard = (props) => {
   return (
-    <React.Fragment>
-      <Wrapper onClick={props.handleMixCloudClick} key={props.key}>
-        <Inner>
-          <Date>{props.date}</Date>
-          <ShowName>{props.showName}</ShowName>
-          <TagWrapper>
-            {props.tags.map((tag, i) => {
-              return (
-                <ShowTag key={i} index={i} name={tag.name} url={tag.url} />
-              );
-            })}
-          </TagWrapper>
-        </Inner>
-      </Wrapper>
-    </React.Fragment>
+    <Inner onClick={props.handleMixCloudClick} key={props.key}>
+      <Date>{props.date}</Date>
+      <ShowName>{props.showName}</ShowName>
+      <TagWrapper>
+        {props.tags.map((tag, i) => {
+          return <ShowTag key={i} index={i} name={tag.name} url={tag.url} />;
+        })}
+      </TagWrapper>
+    </Inner>
   );
 };
 
 const Wrapper = styled.div`
-  border-bottom: 2px solid ${Colors.ehfmPrimary};
   margin: 10px 10px 0;
   padding: 5px;
   color: ${Colors.notQuiteBlack()};
@@ -49,27 +42,28 @@ const Wrapper = styled.div`
 `;
 
 const Inner = styled.div`
-  display: flex;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  border-bottom: 2px solid ${Colors.ehfmPrimary};
 `;
 
 const Date = styled(Body)`
+  grid-column: 1 / 2;
   margin-right: 1rem;
-  flex: 0.25;
   display: flex;
 `;
 
 const ShowName = styled(Heading4)`
+  grid-column: 2 / 3;
   font-weight: normal;
   margin-right: 0.5rem;
   display: flex;
-  flex: 1.5;
 `;
 
 const TagWrapper = styled.div`
+  grid-column: 3 / 3;
   flex-wrap: wrap;
   display: flex;
-  flex: 0.25;
 `;
 
 export default PastShowCard;
