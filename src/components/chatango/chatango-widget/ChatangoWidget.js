e6481d3d51877dc03688510bdf88e13383816ca6
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-08-08 11:07:38 +0100

    Preventing multiple chatango calls

--- src/components/chatango/chatango-widget/ChatangoWidget.js
+++ src/components/chatango/chatango-widget/ChatangoWidget.js
@@ -1,28 +1,25 @@
-import React, { useEffect } from "react";
-import styled from "styled-components/macro";
+import React, { useEffect } from 'react';
+import styled from 'styled-components/macro';
 
 const ChatangoWidget = ({}) => {
   useEffect(() => {
-    console.log("render");
-    const script = document.createElement("script");
-    // make conditional render to prevent creating shitload of script elements
+    const script = document.createElement('script');
     const body = document.body;
-    script.type = "text/javascript";
-    script.src = "https://st.chatango.com/js/gz/emb.js";
+    script.type = 'text/javascript';
+    script.src = 'https://st.chatango.com/js/gz/emb.js';
     // script.dataCfasync = false;
-    script.id = "cid0020000257371229541";
-    script.style.cssText = "width:350px; height:349px;";
+    script.id = 'cid0020000257371229541';
+    // script.dataid = 'chatango-script';
+    script.style.cssText = 'width:350px; height:349px;';
     script.async = true;
     script.text =
       '{"handle":"eh-fm","arch":"js","styles":{"a":"00918a","b":100,"c":"FFFFFF","d":"FFFFFF","k":"00918a","l":"00918a","m":"00918a","n":"FFFFFF","p":"10","q":"00918a","r":100,"pos":"bl","cv":1,"cvfnt":"Helvetica Neue, Helvetica, Arial, sans-serif, sans-serif","cvbg":"00918a","cvw":75,"cvh":30 }}';
-
     body.appendChild(script);
 
     return () => {
-      console.log("unrender");
       document.body.removeChild(script);
     };
-  });
+  }, []);
 
   return null;
 };
