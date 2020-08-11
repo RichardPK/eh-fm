d8085c600f728a8c7aacc757f47673f2e78e01a1
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-08-11 08:48:43 +0100

    Fixed google anayltics call

--- src/components/analytics/ClickEventAnalytics.js
+++ src/components/analytics/ClickEventAnalytics.js
@@ -1,21 +1,25 @@
 import ReactGA from 'react-ga';
 
-export const clickedPlay = ReactGA.event({
-  category: 'Player',
-  action: 'Playing'
-});
+export const clickedPlay = () =>
+  ReactGA.event({
+    category: 'Player',
+    action: 'Playing'
+  });
 
-export const clickedStop = ReactGA.event({
-  category: 'Player',
-  action: 'Stopped'
-});
+export const clickedStop = () =>
+  ReactGA.event({
+    category: 'Player',
+    action: 'Stopped'
+  });
 
-export const clickedMute = ReactGA.event({
-  category: 'Player',
-  action: 'Muted'
-});
+export const clickedMute = () =>
+  ReactGA.event({
+    category: 'Player',
+    action: 'Muted'
+  });
 
-export const clickedUnmute = ReactGA.event({
-  category: 'Player',
-  action: 'Unmuted'
-});
+export const clickedUnmute = () =>
+  ReactGA.event({
+    category: 'Player',
+    action: 'Unmuted'
+  });
