602aa71d80348bf3e3853fd350e41a8bad1d621a
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2020-10-21 20:41:56 +0100

    Update logo on desktop and default description

--- src/components/nav-bar/logo/Logo.js
+++ src/components/nav-bar/logo/Logo.js
@@ -1,15 +1,20 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../../assets/svgs/ehfm-logo.svg";
-import Colors from "../../../consts/Colors";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../../assets/svgs/ehfm-logo.svg';
+import Colors from '../../../consts/Colors';
+import ComRadioLogo from '../../../assets/images/EHFM LOGO _Community Radio White.png';
 
 const LogoHead = ({ mobile, color }) => {
   return (
     <React.Fragment>
       <Wrapper>
         <Link to="/">
-          <StyledLogo color={color} mobile={mobile} />
+          {mobile ? (
+            <StyledLogo color={color} mobile={mobile} />
+          ) : (
+            <LogoImg src={ComRadioLogo} mobile={mobile} />
+          )}
         </Link>
       </Wrapper>
     </React.Fragment>
@@ -21,12 +26,17 @@ const Wrapper = styled.div`
 `;
 
 const StyledLogo = styled(Logo)`
-  height: ${(props) => (props.mobile ? "1.75rem" : "2.5rem")};
+  height: ${(props) => (props.mobile ? '1.75rem' : '2.5rem')};
   width: auto;
   path,
   polygon {
     fill: ${(props) => (props.color ? props.color : Colors.playerWhite)};
   }
+`;
+
+const LogoImg = styled.img`
+  height: ${(props) => (props.mobile ? '2.75rem' : '6rem')};
+  width: auto;
 `;
 
 export default LogoHead;
